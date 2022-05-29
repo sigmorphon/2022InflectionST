0734942ac2342b2f963a540055667ada537deadc
jkodner05 <jkodner05@yahoo.com>
2022-05-29 13:12:49 -0400

    evaluate.py outputs unseen+lemma and both+feats averages

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -14,7 +14,7 @@ def read_train(trainfname, pos):
     with open(trainfname, "r") as ftrain:
         for line in ftrain:
             lemma, infl, feats = line.split("\t")
-            if pos.strip() and pos not in feats.split(";"):
+            if pos and pos not in feats.split(";"):
                 continue
             trainlemmas.add(lemma.strip())
             trainfeats.add(feats.strip())
@@ -30,7 +30,7 @@ def read_eval(evalfname, pos):
             if not line.strip():
                 continue
             lemma, infl, feats = line.split("\t")
-            if pos.strip() and pos not in feats.split(";"):
+            if pos and pos not in feats.split(";"):
                 continue
             evallemmas.append(lemma.strip())
             evalinfls.append(infl.strip())
@@ -54,7 +54,7 @@ def evaluate(lang, predfname, trainfname, evalfname, pos):
 
     if len(predlemmas) != len(evallemmas):
         print("PREDICTION (%d) AND EVAL (%d) FILES HAVE DIFFERENT LENGTHS. SKIPPING %s..." % (len(predlemmas), len(evallemmas), lang))
-        return -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,[],[],[],[],[]
+        return -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,[],[],[],[],[],[],[]
 
     predictions = [int(unicodedata.normalize("NFC",pred)==unicodedata.normalize("NFC",gold)) for pred, gold in zip(predinfls, evalinfls)]
 
@@ -70,11 +70,13 @@ def evaluate(lang, predfname, trainfname, evalfname, pos):
 #        return -1,-1,-1,-1, -1,-1,-1,-1
 
     total_acc = get_acc(predictions)
+    both_feats_acc = get_acc(seenfeats_preds + seenboth_preds)
+    lemma_unseen_acc = get_acc(seenlemma_preds + unseen_preds)
     seenlemma_acc = get_acc(seenlemma_preds)
     seenfeats_acc = get_acc(seenfeats_preds)
     seenboth_acc = get_acc(seenboth_preds)
     unseen_acc = get_acc(unseen_preds)
-    return total_acc, seenboth_acc,seenlemma_acc, seenfeats_acc, unseen_acc, len(predictions), len(seenboth_preds), len(seenlemma_preds), len(seenfeats_preds), len(unseen_preds), predictions, seenboth_preds,seenlemma_preds, seenfeats_preds, unseen_preds
+    return total_acc, both_feats_acc, lemma_unseen_acc, seenboth_acc,seenlemma_acc, seenfeats_acc, unseen_acc, len(predictions), len(seenboth_preds)+len(seenfeats_preds), len(seenlemma_preds)+len(unseen_preds), len(seenboth_preds), len(seenlemma_preds), len(seenfeats_preds), len(unseen_preds), predictions, seenfeats_preds+seenboth_preds, seenlemma_preds+unseen_preds, seenboth_preds,seenlemma_preds, seenfeats_preds, unseen_preds
 
 
 def evaluate_all(predfnames, trainfnames, evalfnames, partitions, pos):
@@ -82,13 +84,17 @@ def evaluate_all(predfnames, trainfnames, evalfnames, partitions, pos):
     def rnd(num):
         return round(100*num, 3)
 
-    print("Lang\t\tall acc\tboth\tlemma\tfeats\tunseen\t\t#total\t#both\t#lemma\t#feats\t#unseen")
+    print("Lang\t\tall acc\tboth+feats\tlemma+unseen\tboth\tlemma\tfeats\tunseen\t\t#total\t#both\t#lemma\t#feats\t#unseen")
     allpredictions = []
+    allpreds_both_feats = []
+    allpreds_lemma_unseen = []
     allpreds_both = []
     allpreds_lemma = []
     allpreds_feats = []
     allpreds_unseen = []
     part_predictions = {part:[] for part in partitions}
+    part_preds_both_feats = {part:[] for part in partitions}
+    part_preds_lemma_unseen = {part:[] for part in partitions}
     part_preds_both = {part:[] for part in partitions}
     part_preds_lemma = {part:[] for part in partitions}
     part_preds_feats = {part:[] for part in partitions}
@@ -98,8 +104,10 @@ def evaluate_all(predfnames, trainfnames, evalfnames, partitions, pos):
         try:
             trainfname = trainfnames[lang]
             evalfname = evalfnames[lang.split("_")[0]]
-            total_acc, seenboth_acc, seenlemma_acc, seenfeats_acc, unseen_acc, num_predictions, num_seenboth, num_seenlemma_preds, num_seenfeats_preds, num_unseen_preds, predictions, seenboth_preds, seenlemma_preds, seenfeats_preds, unseen_preds = evaluate(lang, predfname, trainfname, evalfname, pos)
+            total_acc, both_feats_acc, lemma_unseen_acc, seenboth_acc, seenlemma_acc, seenfeats_acc, unseen_acc, num_predictions, num_both_feats, num_lemma_unseen,num_seenboth, num_seenlemma_preds, num_seenfeats_preds, num_unseen_preds, predictions, both_feats_preds, lemma_unseen_preds, seenboth_preds, seenlemma_preds, seenfeats_preds, unseen_preds = evaluate(lang, predfname, trainfname, evalfname, pos)
             allpredictions.extend(predictions)
+            allpreds_both_feats.extend(both_feats_preds)
+            allpreds_lemma_unseen.extend(lemma_unseen_preds)
             allpreds_both.extend(seenboth_preds)
             allpreds_lemma.extend(seenlemma_preds)
             allpreds_feats.extend(seenfeats_preds)
@@ -107,23 +115,27 @@ def evaluate_all(predfnames, trainfnames, evalfnames, partitions, pos):
             for part in partitions:
                 if part in lang:
                     part_predictions[part].extend(predictions)
+                    part_preds_both_feats[part].extend(both_feats_preds)
+                    part_preds_lemma_unseen[part].extend(lemma_unseen_preds)
                     part_preds_both[part].extend(seenboth_preds)
                     part_preds_lemma[part].extend(seenlemma_preds)
                     part_preds_feats[part].extend(seenfeats_preds)
                     part_preds_unseen[part].extend(unseen_preds)
             if num_predictions == 0:
                 continue
-            print("%s\t%s\t%s\t%s\t%s\t%s\t\t%s\t%s\t%s\t%s\t%s" % (lang, rnd(total_acc), rnd(seenboth_acc), rnd(seenlemma_acc), rnd(seenfeats_acc), rnd(unseen_acc), num_predictions, num_seenboth, num_seenlemma_preds, num_seenfeats_preds, num_unseen_preds))
+            print("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t\t%s\t%s\t%s\t%s\t%s" % (lang, rnd(total_acc), rnd(both_feats_acc), rnd(lemma_unseen_acc), rnd(seenboth_acc), rnd(seenlemma_acc), rnd(seenfeats_acc), rnd(unseen_acc), num_predictions, num_seenboth, num_seenlemma_preds, num_seenfeats_preds, num_unseen_preds))
         except KeyError:
             print("ORIGINAL DATA FOR %s NOT FOUND. SKIPPING..." % lang)
 
     for part in partitions:
         total_acc = get_acc(part_predictions[part])
+        both_feats_acc = get_acc(part_preds_both_feats[part])
+        lemma_unseen_acc = get_acc(part_preds_lemma_unseen[part])
         seenboth_acc = get_acc(part_preds_both[part])
         seenlemma_acc = get_acc(part_preds_lemma[part])
         seenfeats_acc = get_acc(part_preds_feats[part])
         unseen_acc = get_acc(part_preds_unseen[part])
-        print("%s\t\t%s\t%s\t%s\t%s\t%s" % (part.upper(), rnd(total_acc), rnd(seenboth_acc), rnd(seenlemma_acc), rnd(seenfeats_acc), rnd(unseen_acc)))
+        print("%s\t\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (part.upper(), rnd(total_acc), rnd(both_feats_acc), rnd(lemma_unseen_acc), rnd(seenboth_acc), rnd(seenlemma_acc), rnd(seenfeats_acc), rnd(unseen_acc)))
 
     total_acc = get_acc(allpredictions)
     seenboth_acc = get_acc(allpreds_both)
